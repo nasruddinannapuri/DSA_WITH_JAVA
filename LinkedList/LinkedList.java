598332f3296f0dd220a8a000e1fbe9b93781d560
Nasruddin Annapuri <nasruddinannapuri@gmail.com>
2024-03-31 01:40:17 +0530

    commit

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -270,23 +270,28 @@ public class LinkedList {
         Node slow = head;
         Node fast = head;
         boolean cycle = false;
-    while(fast != null && fast.next != null){
-        slow = slow.next;
-        fast = fast.next.next;
-        if(fast == slow){
-            cycle = true;
-            break;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
+                cycle = true;
+                break;
+            }
         }
         if(cycle == false){
             return;
         }
-        }
-    }
         // find meeting point
         slow = head;
         Node prev = null;
+        while(slow != fast){
+            prev = fast;
+            slow = slow.next; 
+            fast = fast.next;
+        }
         // remove cycle -> last.next = null;
-         
+        prev.next = null;
+        
     }
 
     public static void main(String[] args) {
@@ -350,12 +355,17 @@ public class LinkedList {
         System.out.println(ll.checkPalindrome());
 
         head = new Node(1);
-        head.next = new Node(2);
+        Node temp = new Node(2);
+        head.next = temp;
         head.next.next = new Node(3);
-        head.next.next.next = head;
+        head.next.next.next = temp;
 
         System.out.println(ll.isCycle());
+        // 1->2->3->2
 
+        ll.removeCycle();
+        System.out.println(ll.isCycle());
+        ll.print();
 
 
     }
---
+++ LinkedList/MergeSort.java
@@ -0,0 +1,9 @@
+package LinkedList;
+import java.util.LinkedList;
+
+public class MergeSort {
+    public static void main(String[] args) {
+    
+    }
+    
+}
